e57af4c2b8781fa0f9f6ff567f312d631ab40de0
André Knob <knobandre@gmail.com>
2018-08-26 23:06:19 -0300

    WIP: Setting request's file structure

--- backend/src/module/user/user.controller.js
+++ backend/src/module/user/user.controller.js
@@ -1,14 +1,61 @@
-var User = require('./user.schema');
+const User = require('./user.schema');
+const userLogic = require('./user.logic');
 
 module.exports = {
-    // Display list of all Users.
+    /**
+     * Fetch a specific User by its id.
+     */ 
+    get: (req, res) => {
+        userLogic.get(req.params.id).then(response => {
+            res.status(response.status).json(response);
+        }).catch(err => {
+            res.status(err.status).json(err);
+        });;
+    },
+
+    /**
+     * Display list of all Users.
+     */ 
     list: (req, res) => {
-        // todo - .find is deprecated
-        User.find({}, (err, users) => {
-            if (err) {
-                return handleError(err);
-            }
-            res.status(200).json(users);
+        User.find((err, users) => {
+            return err ? handleError(err) : res.status(200).json(users);
         });
+    },
+
+    /**
+     * Create a new User.
+     */ 
+    post: (req, res) => {
+        let user = new User({
+            name: req.body.name,
+        });
+        user.save((err, result) => {
+            return err ? handleError(err) : res.status(200).json({msg: 'User created successfully', result});
+        });
+    },
+
+    /**
+     * Update an User.
+     */ 
+    put: (req, res) => {
+        User.findOneAndUpdate(
+            {_id: req.params.id},
+            {$set: {name: req.body.name}}, 
+            (err, result) => {
+                return err ? handleError(err) : res.status(200).json({msg: 'User updated successfully', result});
+            }
+        );
+    },
+
+    /**
+     * Remove an User.
+     */ 
+    delete: (req, res) => {
+        User.remove(
+            {_id: req.params.id},
+            (err, result) => {
+                return err ? handleError(err) : res.status(200).json(result);
+            }
+        );
     }
 }
--- backend/src/route/user.routes.js
+++ backend/src/route/user.routes.js
@@ -1,59 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../module/user/user.controller');
-const User = require('../module/user/user.schema');
 
-router.get('/get/:id', (req, res, next) => {
-    // todo - .find is deprecated
-    User.find((err, users) => {
-        if (err) {
-            return handleError(err);
-        }
-        res.status(200).json(users);
-    });
-});
+router.get('/get/:id', userController.get);
 
 router.get('/list', userController.list);
 
-router.post('/post', (req, res, next) => {
-    let user = new User({
-        name: req.body.name,
-    });
-    user.save((err, result) => {
-        if (err) {
-            return handleError(err);
-        }
-        res.status(200).json({msg: 'User created successfully', result});
-    });
-});
+router.post('/post', userController.post);
 
-router.put('/put/:id', (req, res, next) => {
-    User.findOneAndUpdate(
-        {_id: req.params.id},
-        {$set: {name: req.body.name}}, 
-        (err, result) => {
-            if (err) {
-                return handleError(err);
-            }
-            res.status(200).json({msg: 'User updated successfully', result});
-        }
-    );
-});
+router.put('/put/:id', userController.put);
 
-router.delete('/delete/:id', (req, res, next) => {
-    User.remove(
-        {_id: req.params.id},
-        (err, result) => {
-            if (err) {
-                return handleError(err);
-            }
-            res.status(200).json(result);
-        }
-    );
-});
-
-function handleError() {
-    // todo
-}
+router.delete('/delete/:id', userController.delete);
 
 module.exports = router;
